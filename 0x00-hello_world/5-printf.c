d35032a58b9c0f1a93b3b120a36fd6335e9d477f
agathalebyane <agathalebyane7@gmail.com>
2023-04-20 11:55:59 -0700

    101-quote.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome id a piece of art,\n"):
-		return (0);
-i}
+	printf("with proper grammar . but the outcome id a piece of art,\n"):
+		return (0)e
+}
