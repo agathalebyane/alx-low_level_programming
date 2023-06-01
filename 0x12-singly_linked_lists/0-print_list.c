cd25010500872a5fa4f35238add19126e0ca4866
agathalebyane <agathalebyane7@gmail.com>
2023-06-01 12:06:59 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,63 +1,27 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * struct list_s - Singly linked list
- * @str: String - (malloc'ed string)
- * @len: Length of the string
- * @next: Points to the next node
+ * display_list - Displays all the elements of a linked list.
+ * @head: Pointer to the list_t list to display.
  *
- * Description: Singly linked list node structure
- * for Holberton project
+ * Return: The number of nodes displayed.
  */
-typedef struct list_s
+size_t display_list(const list_t *head)
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	size_t total_count = 0;
 
-/**
- * print_list - Prints all the elements of a linked list.
- * @h: The head of the linked list.
- *
- * Return: The number of nodes in the list.
- */
-size_t print_list(const list_t *h);
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+			printf("[0] (none)\n");
+		else
+			printf("[%u] %s\n", head->len, head->str);
 
-/**
- * list_len - Returns the number of elements in a linked list.
- * @h: The head of the linked list.
- *
- * Return: The number of elements in the list.
- */
-size_t list_len(const list_t *h);
+		head = head->next;
+		total_count++;
+	}
 
-/**
- * add_node - Adds a new node at the beginning of a linked list.
- * @head: The head of the linked list.
- * @str: The string to be stored in the new node.
- *
- * Return: The address of the new node, or NULL if it failed.
- */
-list_t *add_node(list_t **head, const char *str);
-
-/**
- * add_node_end - Adds a new node at the end of a linked list.
- * @head: The head of the linked list.
- * @str: The string to be stored in the new node.
- *
- * Return: The address of the new node, or NULL if it failed.
- */
-list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * free_list - Frees a linked list.
- * @head: The head of the linked list.
- */
-void free_list(list_t *head);
-
-#endif /* LISTS_H */
+	return (total_count);
+}
 
